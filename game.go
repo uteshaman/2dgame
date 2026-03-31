2dd88dc161915aa7c171c6737163bd2353912d71
aman <110390405+uteshaman@users.noreply.github.com>
2026-03-31 14:03:33 +0500

    Update game.go

--- game.go
+++ game.go
@@ -1,89 +1,114 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+)
+
 type Game struct {
 	board       [][]int
 	pX          int
 	pY          int
 	boardHeight int
 	boardWidth  int
-	Direction string 
+	Direction   string
+	Score       int
+	FoodX       int
+	FoodY       int
 }
 
-func NewGame() *Game {
-	g := new(Game)
-
-	g.boardHeight = 16
-	g.boardWidth = 16
-
+// NewGame енді параметр қабылдайды
+func NewGame(width, height int) *Game {
+	g := &Game{
+		boardHeight: height,
+		boardWidth:  width,
+	}
 	g.ResetGame()
-
+	rand.Seed(time.Now().UnixNano())
+	g.PlaceFood()
 	return g
-
 }
-func (g *Game) resetGame() {
+
+// Тақтаны бастапқы күйге келтіру
+func (g *Game) ResetGame() {
 	g.board = make([][]int, g.boardHeight)
-
 	for i := 0; i < g.boardHeight; i++ {
-		g.board[i] = make([][]int, g.boardWidth)
-
-		for j := 0; j < g.boardHeight; j++ {
+		g.board[i] = make([]int, g.boardWidth)
+		for j := 0; j < g.boardWidth; j++ {
 			g.board[i][j] = 0
 		}
 	}
 	g.board[0][0] = 1
 	g.pX = 0
 	g.pY = 0
-
+	g.Score = 0
 }
+
+// Қозғалыс функциялары
 func (g *Game) MoveUp() {
-	g.board[g.pX][g.pY] = 0
-	g.pX--
-	g.board[g.pX][g.pY] = 1
+	g.tryMove(-1, 0)
 }
-func (g *Game) MoveRight() {
-	g.board[g.pX][g.pY] = 0
-	g.pY++
-	g.board[g.pX][g.pY] = 1
-}
+
 func (g *Game) MoveDown() {
-	g.board[g.pX][g.pY] = 0
-	g.pX++
-	g.board[g.pX][g.pY] = 1
+	g.tryMove(1, 0)
 }
+
 func (g *Game) MoveLeft() {
-	g.board[g.pX][g.pY] = 0
-	g.pY--
-	g.board[g.pX][g.pY] = 1
+	g.tryMove(0, -1)
 }
 
-for (g *Game) CheckMOve() bool{
-	switch g.Direction{
+func (g *Game) MoveRight() {
+	g.tryMove(0, 1)
+}
 
-	case "up":
-		if g.pX == 0 || g.board[g.pX-1][g.pY] == 1{
-			return false
-		}
-		return true 
-		
-	case "right":
-		if g.pY == g.boardHeight-1 || g.board[g.pX][g.pY+1] == 1{
-			return false
-		}
-		return true
+// Жалпы қозғалыс функциясы
+func (g *Game) tryMove(dx, dy int) {
+	newX := g.pX + dx
+	newY := g.pY + dy
 
-	case "down":
-		if g.pX == g.boardWidth-1 || g.board[g.pX+1][g.pY] == 1{
-			return false
-		}
-		return true 
+	// шекара және өзіне соғу
+	if newX < 0 || newX >= g.boardHeight || newY < 0 || newY >= g.boardWidth {
+		return
+	}
 
-	case "left":
-		if g.pY == 0 || g.board[g.pX][g.pY-1] == 1{
-			return false
+	// Жаңарту
+	g.board[g.pX][g.pY] = 0
+	g.pX = newX
+	g.pY = newY
+	g.board[g.pX][g.pY] = 1
+
+	// Food тексеру
+	if g.pX == g.FoodX && g.pY == g.FoodY {
+		g.Score++
+		g.PlaceFood()
+	}
+}
+
+// Food орналастыру
+func (g *Game) PlaceFood() {
+	for {
+		x := rand.Intn(g.boardHeight)
+		y := rand.Intn(g.boardWidth)
+		if g.board[x][y] == 0 {
+			g.FoodX = x
+			g.FoodY = y
+			break
 		}
-		return true
+	}
+}
+
+// Дирекция тексеру (main.go үшін қажет болса)
+func (g *Game) CheckMove() bool {
+	switch g.Direction {
+	case "Up":
+		return g.pX > 0 && g.board[g.pX-1][g.pY] != 1
+	case "Right":
+		return g.pY < g.boardWidth-1 && g.board[g.pX][g.pY+1] != 1
+	case "Down":
+		return g.pX < g.boardHeight-1 && g.board[g.pX+1][g.pY] != 1
+	case "Left":
+		return g.pY > 0 && g.board[g.pX][g.pY-1] != 1
 	default:
-		panic("OMG!!! Direction is wrong")
-	
+		return false
 	}
 }
