a767cae459cc08ca3ba76c558c5df6a59bfe2d57
aman <110390405+uteshaman@users.noreply.github.com>
2026-03-31 14:02:42 +0500

    Update render.go

--- render.go
+++ render.go
@@ -1,10 +1,14 @@
 package main
 
-import "github.com/termbox-go"
+import (
+	"strconv"
+
+	"github.com/nsf/termbox-go"
+)
 
 const backgroundColor = termbox.ColorBlue
 const boardColor = termbox.ColorBlack
-const instructionColor = termbox.COlorWhite
+const instructionColor = termbox.ColorWhite
 
 const defaultMarginWidth = 2
 const defaultMarginHeight = 1
@@ -20,17 +24,21 @@ const title = "Game"
 
 func (g *Game) Render() {
 	termbox.Clear(backgroundColor, backgroundColor)
+	// Тақырып
 	termboxPrint(titleStartX, titleStartY, instructionColor, backgroundColor, title)
 
+	// Ойын тақтасы
 	for i := 0; i < g.boardHeight; i++ {
 		for j := 0; j < g.boardWidth; j++ {
 			var cellColor termbox.Attribute
 
-			switch g.board[i][j] {
-			case 0:
-				cellColor = termbox.ColorBlack
-			case 1:
+			// Food көрсету
+			if i == g.FoodX && j == g.FoodY {
+				cellColor = termbox.ColorRed
+			} else if g.board[i][j] == 1 {
 				cellColor = termbox.ColorGreen
+			} else {
+				cellColor = boardColor
 			}
 
 			for x := 0; x < cellWidth; x++ {
@@ -38,12 +46,17 @@ func (g *Game) Render() {
 			}
 		}
 	}
+
+	// Score көрсету
+	scoreMsg := "Score: " + strconv.Itoa(g.Score)
+	termboxPrint(boardStartX, boardStartY+g.boardHeight+1, instructionColor, backgroundColor, scoreMsg)
+
 	termbox.Flush()
 }
 
-func termboxPrint(x, y int, fg, bg ternbox.Attribute, msg string) {
+func termboxPrint(x, y int, fg, bg termbox.Attribute, msg string) {
 	for _, c := range msg {
-		termbox.SetCell(x, y, fg, bg)
+		termbox.SetCell(x, y, c, fg, bg)
 		x++
 	}
 }
